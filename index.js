f3201ab86479b563b4b1a8f9bf53e3109d2e0841
jim <jbmbroomfield@gmail.com>
2021-03-08 15:35:36 +1100

    Complete

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Write your solution in this file!
+let employee = {
+    name: "Jim",
+    streetAddress: "1 Smith St",
+};
+
+
+function updateEmployeeWithKeyAndValue(emp,key,value) {
+    return Object.assign({...emp},{[key]:value});
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue (emp,key,value) {
+    emp[key] = value;
+    return emp;
+}
+
+
+function destructivelyDeleteFromEmployeeByKey(emp,key) {
+    delete emp[key];
+    return emp;
+}
+
+function deleteFromEmployeeByKey(emp,key) {
+    return destructivelyDeleteFromEmployeeByKey({...emp},key);
+}
